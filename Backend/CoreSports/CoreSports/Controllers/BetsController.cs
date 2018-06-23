001506b85146dea4ab30609cf4aa910ef71fc5a2
Teodor92 <teodor.ivanov92@gmail.com>
2018-06-23 20:57:58 +0300

    Added gitignore file.

---
+++ Backend/CoreSports/CoreSports/Controllers/BetsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreSports.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Bets")]
+    public class BetsController : Controller
+    {
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
