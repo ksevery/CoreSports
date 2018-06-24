63db6c1e694b7b3c341694dc6388372736165f5e
Teodor92 <teodor.ivanov92@gmail.com>
2018-06-24 10:43:22 +0300

    Added mapping service for the XML.

---
+++ Backend/CoreSports/CoreSports/Services/Contracts/IMappingService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Models;
+
+namespace CoreSports.Services.Contracts
+{
+    public interface IMappingServicecs
+    {
+        IEnumerable<Event> MapToEvents(Stream documentStream);
+    }
+}
