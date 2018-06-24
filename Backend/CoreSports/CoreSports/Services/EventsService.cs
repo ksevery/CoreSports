b5f5933e025735a1c5bf72d091efd7159fdc4e9b
Teodor92 <teodor.ivanov92@gmail.com>
2018-06-24 13:33:21 +0300

    Added update XML upload functionality.

--- Backend/CoreSports/CoreSports/Services/EventsService.cs
+++ Backend/CoreSports/CoreSports/Services/EventsService.cs
@@ -41,6 +41,62 @@ namespace CoreSports.Services
 
         private void UpdateModels(IList<Event> models)
         {
+            foreach (var model in models)
+            {
+                var databaseEvent =
+                    this.unitOfWork.Events.Entities.FirstOrDefault(x => x.InternalId == model.InternalId);
+
+                this.UpdateEvents(databaseEvent, model);
+            }
+
+            this.unitOfWork.Commit();
+        }
+
+        private void UpdateEvents(Event databaseEvent, Event updateEvent)
+        {
+            if (databaseEvent != null)
+            {
+                databaseEvent.Away = updateEvent.Away;
+                databaseEvent.Home = updateEvent.Home;
+                databaseEvent.Time = updateEvent.Time;
+                databaseEvent.Type = updateEvent.Type;
+
+                foreach (var market in updateEvent.Markets)
+                {
+                    var databaseMarket =
+                        this.unitOfWork.Markets.Entities.FirstOrDefault(x => x.InternalId == market.InternalId);
+
+                    this.UpdateMarket(databaseMarket, market);
+                }
+            }
+        }
+
+        private void UpdateMarket(Market databaseMarket, Market updateMarket)
+        {
+            if (databaseMarket != null)
+            {
+                databaseMarket.Name = updateMarket.Name;
+                databaseMarket.Number = updateMarket.Number;
+                databaseMarket.Status = updateMarket.Status;
+            }
+
+            foreach (var selection in updateMarket.Selections)
+            {
+                var databaseSelection =
+                    this.unitOfWork.Selections.Entities.FirstOrDefault(x => x.InternalId == selection.InternalId);
+                UpdateSelection(databaseSelection, selection);
+            }
+        }
+
+        private static void UpdateSelection(Selection databaseSelection, Selection updateSelection)
+        {
+            if (databaseSelection != null)
+            {
+                databaseSelection.Description = updateSelection.Description;
+                databaseSelection.Number = updateSelection.Number;
+                databaseSelection.Odds = updateSelection.Odds;
+                databaseSelection.ParticipantType = updateSelection.ParticipantType;
+            }
         }
 
         public IEnumerable<EventViewModel> MapToViewModels(IEnumerable<Event> models)
@@ -60,6 +116,7 @@ namespace CoreSports.Services
                     Name = y.Name,
                     EventId = y.EventId,
                     Number = y.Number,
+                    Status = y.Status,
                     Selections = y.Selections.Select(z => new SelectionViewModel
                     {
                         Id = z.Id,
